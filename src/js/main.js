4fbba5d29cec499a6409a4aff3b7c19b2ecff85c
MaksymBora <maxboraod@gmail.com>
2023-06-29 17:59:15 +0200

    add libraries

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import card from '../templates/card.hbs';
+// import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 
-const axios = require('axios').default;
+// const axios = require('axios').default;
