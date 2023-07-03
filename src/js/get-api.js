6c12af9abff310d8eb2f15f707705e6f48ce0e0c
MaksymBora <maxboraod@gmail.com>
2023-07-03 23:56:21 +0200

    update key

--- src/js/get-api.js
+++ src/js/get-api.js
@@ -7,7 +7,9 @@ const refs = getRefs();
 export let totalPerPage = 40;
 
 // getting API
-const API_KEY = '20403084-138caa44d9b5066c1dd91e458';
+
+// 20403084-138caa44d9b5066c1dd91e458
+const API_KEY = '38051155-55c7a09781d6cf5219707538c';
 const url = 'https://pixabay.com/api/';
 
 // API data request
